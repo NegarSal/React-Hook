7f12ebb45a79dd173f0d723d6d550d46f58433f2
Negar <negarsaljooghi@gmail.com>
2024-07-23 14:20:40 +0330

    useEffect with cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
     <div className="App">
-      <HookMouse />
+      <MouseContainer />
     </div>
   );
 }
